e94d323648a3a0d6a2ea958a438e72c1a273e483
Hassan A <has_is_ere@hotmail.com>
2018-07-23 10:38:16 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -8,7 +8,13 @@ function frameIt(){
   $('img').addClass("tasty")
 }
 
-  
+function pressIt(){
+  $('input').on("keydown", function(key){
+    if(key.which==83){
+      alert('G was pressed')
+    }
+  })
+}
   
 
 $(document).ready(function(){
