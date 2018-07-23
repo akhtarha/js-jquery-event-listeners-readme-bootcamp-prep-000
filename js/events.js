5db01a1e9daedda1855ee33fbe272b88cdf28781
Hassan A <has_is_ere@hotmail.com>
2018-07-23 10:46:57 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -18,7 +18,7 @@ function pressIt(){
 
 function submitIt(){
   $("form").on("submit", function(){
-    console.log("Your form is going to be submitted now.")
+    alert("Your form is going to be submitted now.")
   })
 }
 
